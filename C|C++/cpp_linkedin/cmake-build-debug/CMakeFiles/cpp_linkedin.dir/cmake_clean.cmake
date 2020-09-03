7289908ad41292489b89355ab47e814b618382e5
laeberkaes <huberhan@hs-albsig.de>
2020-09-03 22:02:06 +0200

    c++

---
+++ C|C++/cpp_linkedin/cmake-build-debug/CMakeFiles/cpp_linkedin.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpp_linkedin.dir/main.cpp.o"
+  "cpp_linkedin"
+  "cpp_linkedin.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpp_linkedin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

38011ceb4f57eb4fc5cf63ef46fc72b26d2e8ed4
hnshbr <huberhan@hs-albsig.de>
2020-09-06 12:43:00 +0200

    PEP
    Signed-off-by: hnshbr <huberhan@hs-albsig.de>

--- Python/adventure/beta/lib/room1.py
+++ Python/adventure/beta/lib/room1.py
@@ -1,11 +1,15 @@
-from lib.static import screen_width, clear, speech_manipulation, confirmation
-import random
 import time
-class P():
+
+from lib.static import clear, speech_manipulation, confirmation
+
+
+class P:
     def __init__(self):
         self.name = "Testy McTesterton"
         self.location = "a2"
-class Room1():
+
+
+class Room1:
     def __init__(self, player):
         self.intro(player)
         self.story_setup()
@@ -18,23 +22,24 @@ class Room1():
         print("You are free. You walk around a bit and find your way to your house.")
         player.location = "b2"
 
-    def push_obj(self,l):
+    def push_obj(self, l):
         if l[0] not in self.found_objects:
             self.found_objects += l
 
-    def get_obj(self,o):
+    def get_obj(self, o):
         if o not in self.inventory:
             self.inventory.append(o)
 
     def intro(self, player):
-        print("Hey!", end = " ")
+        print("Hey!", end=" ")
         time.sleep(1)
         print(" " + player.name)
         time.sleep(1)
         speech_manipulation("Come over here for a second, I have an offer for you.", 0.08)
         time.sleep(1)
         clear()
-        print("You look like a trustworthy adventure. I have a problem, you see, and I need help. My farm was raided by some bandits and I have nothing left to eat.")
+        print(
+            "You look like a trustworthy adventure. I have a problem, you see, and I need help. My farm was raided by some bandits and I have nothing left to eat.")
         time.sleep(3)
         print("They left a note with a message. I have it in my house. Would you come take a look and help me?")
         ant = str(input("> ")).lower()
@@ -45,13 +50,14 @@ class Room1():
             speech_manipulation("Thank you!!", 0.1)
             time.sleep(1)
             print(" ")
-            speech_manipulation("** You follow the man through a narrow street. After you turn a corner everything is black!", 0.07)
+            speech_manipulation(
+                "** You follow the man through a narrow street. After you turn a corner everything is black!", 0.07)
             time.sleep(1)
             clear()
             time.sleep(2)
 
     def story_setup(self):
-        #player.location = "room_1"
+        # player.location = "room_1"
         speech_manipulation("** You wake up. Your head hurts like hell.\n", 0.075)
         time.sleep(1)
         speech_manipulation("** You feel your pockets. Empty.\n", 0.075)
@@ -64,7 +70,7 @@ class Room1():
         print("You look around the room. You can see:")
         found = "A big wooden door, a cupboard, a table, a chair"
         for i in self.found_objects:
-            found += ", "+i
+            found += ", " + i
         print(found)
         if self.inventory:
             print("You have:")
@@ -75,7 +81,8 @@ class Room1():
         print("\n" + "=" * len("What would you like to do?"))
         print("What would you like to do?\n")
         print("-" * len("What would you like to do?"))
-        print(" " * (int((len("What would you like to do?") - len("examine, take, put, look, etc.")) / 2)) + "examine, help, etc.")
+        print(" " * (int(
+            (len("What would you like to do?") - len("examine, take, put, look, etc.")) / 2)) + "examine, help, etc.")
         print("-" * len("What would you like to do?") + "\n")
         action = str(input("> ")).lower().split()
         acceptable_actions = ["help", "examine", "turn", "open", "break"]
@@ -147,30 +154,39 @@ class Room1():
         if not self.solved:
             self.room_prompt()
 
+
 class Table():
     def __init__(self):
         self.name = "table"
+
     def examine(self, room):
         print("You see a table with a notebook on top and a big drawer.")
         room.push_obj(["notebook", "drawer"])
 
+
 class Notebook():
     def __init__(self):
         self.name = "notebook"
-    def turn(self,room):
+
+    def turn(self, room):
         print("You turn it around. There is nothing on its back.")
+
     def examine(self, room):
-        print("You open the notebook. You read the first lines of the notebook: 'The Earth is the Center.' A piece of paper falls out.")
+        print(
+            "You open the notebook. You read the first lines of the notebook: 'The Earth is the Center.' A piece of paper falls out.")
         room.push_obj(["paper"])
         room.clues_found += 1
+
 
 class Chair():
     def __init__(self):
         self.name = "chair"
         self.broken = False
+
     def examine(self, room):
-        print("Looks like an old wooden chair.")#+" You can see nothing on the seat.")
-    def turn(self,room):
+        print("Looks like an old wooden chair.")  # +" You can see nothing on the seat.")
+
+    def turn(self, room):
         s = """
     +   +
   v | i | r 
@@ -182,6 +198,7 @@ class Chair():
 """
         print("On the bottom of the chair you find strange markings." + s)
         room.clues_found += 1
+
     def breaking(self, room):
         if not self.broken:
             print("You broke the chair into pieces.")
@@ -191,12 +208,15 @@ class Chair():
         else:
             print("It's already broken.")
 
+
 class Cupboard():
     def __init__(self):
         self.name = "cupboard"
         self.combination = "2"
+
     def examine(self, room):
         print("An old mahagony wardrobe with big doors. There is a lock on the doors.")
+
     def open(self, room):
         combination = self.combination
         print("In it you find a note.")
@@ -208,21 +228,28 @@ class Cupboard():
         except:
             print("Not quite...")
 
+
 class PieceOfPaper():
     def __init__(self):
         self.name = "paper"
+
     def examine(self, room):
-        print("It reads: 'When the planets allign, take the third of the second, the fourth of the fifth, the last of the sixth and the first of the third.")
+        print(
+            "It reads: 'When the planets allign, take the third of the second, the fourth of the fifth, the last of the sixth and the first of the third.")
         room.clues_found += 1
-    def turn(self,room):
+
+    def turn(self, room):
         print("On its back it reads: 'M, ...'")
+
 
 class Door():
     def __init__(self):
         self.name = "door"
         self.combination = "295"
+
     def examine(self, room):
         print("A scratched metal door with a combination look. You need three digits to open it.")
+
     def open(self, room):
         if room.clues_found >= 4:
             print("If you know the combination, try it now:")
@@ -231,7 +258,8 @@ class Door():
                 speech_manipulation("** You manage to open the door with your combination!\n", 0.06)
                 time.sleep(2)
                 print("In the next room you find all your equipment and a note from your capturer:")
-                print("'Not bad. You are not as dumb as I thought... Maybe you are useful after all. We will be in touch.'")
+                print(
+                    "'Not bad. You are not as dumb as I thought... Maybe you are useful after all. We will be in touch.'")
                 i = """          ▄▀▄
         ▄▀───▀▄
       ▄▀──▄▄▄──▀▄
@@ -246,11 +274,14 @@ class Door():
         else:
             print("Are you sure you've found enough to know the combination?")
 
+
 class Drawer():
     def __init__(self):
         self.name = "drawer"
+
     def examine(self, room):
         print("It looks stuck. You need something to pry it open.")
+
     def open(self, room):
         if "Chair Leg" in room.inventory:
             print("You use the leg of the chair to open the drawer and find a book inside.")
@@ -258,14 +289,17 @@ class Drawer():
         else:
             print("It's stuck...")
 
+
 class Book():
     def __init__(self):
         self.name = "book"
+
     def examine(self, room):
         print("You flip through its pages and on the last page you find a peculiar symbol: ┐ U ┘ ┌")
         room.clues_found += 1
-    def turn(self,room):
+
+    def turn(self, room):
         print("You turn around the book but it has nothing interesting on its back.")
 
-#p = P()
-#Room1 = Room1(p)
+# p = P()
+# Room1 = Room1(p)
